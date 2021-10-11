a79d4a9c4cee08ef0cf92331434307d8f37a4ae4
valyafrankova22 <vfrankova22@icloud.com>
2021-10-11 23:55:36 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const userNum = +prompt(`Введите число`);
+
+function isPrime() {
+    for (let i = 2; i < userNum; i++) {
+        if (userNum % 2 === 0) {
+            return false
+        }
+    }
+    return userNum > 1;
+}
+const result = isPrime;
+console.log(result);
